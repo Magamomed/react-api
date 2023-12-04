3fe8f896c317a2b9ee60b052c160c677a9cdc7c2
Magamomed <saparov-sultan2017@bk.ru>
2023-12-04 18:42:20 +0600

    code refactoring

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './error-indicator.css';
-import icon from './death-star.png';
+import icon from './image_l.jpg';
 
 const ErrorIndicator = () => {
     return (
